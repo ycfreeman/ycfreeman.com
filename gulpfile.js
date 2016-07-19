214d3fa6a9a82b0f02aa90a196b9d3e586b6b2b1
Freeman Man <freeman@ycfreeman.com>
2016-07-20 00:32:28 +1000

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -389,7 +389,11 @@ gulp.task('deploy-gh-pages', function() {
   return gulp.src(dist('**/*'))
     // Check if running task from Travis CI, if so run using GH_TOKEN
     // otherwise run using ghPages defaults.
-    .pipe($.ghPages());
+    .pipe($.if(process.env.TRAVIS === 'true', $.ghPages({
+      remoteUrl: 'https://$GH_TOKEN@github.com/polymerelements/polymer-starter-kit.git',
+      silent: true,
+      branch: 'gh-pages'
+    }), $.ghPages()));
 });
 
 // Load tasks for web-component-tester
