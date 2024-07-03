1af87b24580940ecf30c8cc305103d277d355d7b
Freeman Man <freeman@ycfreeman.com>
2024-07-03 20:43:06 +1000

    set ga id in env var

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -35,7 +35,7 @@ const siteMetadata = {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
     googleAnalytics: {
-      googleAnalyticsId: "G-SGM4PJFR10", // e.g. G-XXXXXXX
+      googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID, // e.g. G-XXXXXXX
     },
   },
   // newsletter: {
